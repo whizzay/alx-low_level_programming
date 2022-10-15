740268ffbe80660bf679e796cd84f2fca215b462
whizzay <officialwhizzay@gmail.com>
2022-10-14 17:57:43 -0700

    variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)/***this specifies there are no arguments taken by main */
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+{
+	putchar(ch);
+}
+putchar(10);
+return (0);
+}
