973d6131bb53901fe91c5defb8a0c43c85fec135
whizzay <officialwhizzay@gmail.com>
2022-11-05 17:14:40 -0700

    static library created

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - checks for lowercase character
+ * @c:The character in letters
+ * Return: 1 and 0 depending on the condition
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
